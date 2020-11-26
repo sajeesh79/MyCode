bc1b0dd9e03c0396bba05c669a345a3322e38526
Sajeesh <ssankarapanickar@salesforce.com>
2020-11-26 09:07:53 +0800

    Testing still

--- Mathematics.py
+++ Mathematics.py
@@ -1,10 +1,10 @@
 from flask import render_template, request, redirect, url_for
 from flask import session
 from Addition_within_100 import gen_random
-from app import app
+from flask import Flask
 
-# app = Flask(__name__)
-
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 no_of_q = 24
 cols = 8
