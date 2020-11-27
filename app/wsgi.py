b403d193f2d2bbd64b0ccdbe99aed160bdb3c110
Sajeesh <ssankarapanickar@salesforce.com>
2020-11-27 21:26:29 +0800

    Testing for uwsgi

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,5 +1,7 @@
 from app import app as application
+import os
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    application.run(host='0.0.0.0', port=port)
 
